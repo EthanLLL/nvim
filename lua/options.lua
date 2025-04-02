ad279f0a4626743ce74acdab95bbd0364e6bc65f
Luo <zhenzluo@b0be837e764e.ant.amazon.com>
2025-04-02 15:32:07 +0800

    first commit

---
+++ lua/options.lua
@@ -0,0 +1,12 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+-- Init setting, remap etc.
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>qq", vim.cmd.q)
